f3e4be258e6573c6d6bb01365df0bc12a583e23d
Daniel Hug <danielthehug@gmail.com>
2015-01-23 00:30:58 -0800

    Fix #15: improve wallets panel

--- js/views/wallet-list/wallet.js
+++ js/views/wallet-list/wallet.js
@@ -4,35 +4,37 @@
 	'use strict';
 
 	// handle clicks on edit buttons
-	function editBtnClick(fieldToToggle) {
-		// disable all but fieldToToggle
-		$.each($.qsa('input', TF.views.walletList.parent), function(field) {
-			if (field === fieldToToggle) return;
-			if (!field.disabled) field.disabled = true;
-		});
-
-		// toggle fieldToToggle's disabled/focused state
+	function editBtnClick(fieldToToggle, btnToToggle) {
+		// toggle disabled state of passed input and passed button
 		fieldToToggle.disabled = !fieldToToggle.disabled;
+		btnToToggle.disabled = !btnToToggle.disabled;
+
+		// toggle input's focused state
 		if (!fieldToToggle.disabled) fieldToToggle.focus();
 	}
 
 	function renderWallet(wallet) {
 		var formattedBalance = $.formatMoney(wallet.balance || 0);
 
-		var editField = DOM.buildNode({ el: 'input', type: 'text', _value: wallet.name, name: 'name', _disabled: true });
+		var editField = DOM.buildNode({ el: 'input', type: 'text', _className: 'wallet-name-edit-field', _value: wallet.name, name: 'name', _disabled: true });
+		var saveBtn = DOM.buildNode({ el: 'button', _className: 'wallet-save-btn', kid: 'save', _disabled: true });
 		var walletRow = DOM.buildNode({ el: 'tr', _id: 'wallet_' + wallet._id, kids: [
-			{ el: 'td', kid:
-				{ el: 'button', _className: 'icon-pencil', on_click: editBtnClick.bind(null, editField), kid:
+			{ el: 'td', _className: 'wallet-action-col', kid:
+				{ el: 'button', _className: 'icon-pencil', on_click: editBtnClick.bind(null, editField, saveBtn), kid:
 					{ el: 'span', _className: 'ir', kid: 'edit' }
 				}
 			},
-			{ el: 'td', kid:
-				{ el: 'form', kid: editField, on_submit: function editFormSubmit(event) {
-					event.preventDefault();
-					TF.views.walletList.edit(wallet, {name: this.name.value});
-				} }
+			{ el: 'td', kid: {
+					el: 'form',
+					on_submit: function editFormSubmit(event) {
+						event.preventDefault();
+						editBtnClick.call(null, editField, saveBtn);
+						TF.views.walletList.edit(wallet, {name: this.name.value});
+					},
+					kids: [editField, saveBtn]
+				}
 			},
-			{ el: 'td', kid: formattedBalance }
+			{ el: 'td', _className: 'wallet-amount-col', kid: formattedBalance }
 		] });
 
 		return walletRow;
