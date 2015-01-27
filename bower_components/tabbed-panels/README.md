84ed7562bd3dce402098a54b3e6fd183537b9579
Daniel Hug <danielthehug@gmail.com>
2015-01-27 10:39:22 -0800

    Fix #16 by updating tabbed-panels

--- bower_components/tabbed-panels/README.md
+++ bower_components/tabbed-panels/README.md
@@ -1,7 +1,43 @@
-tabbed-panels
+tabbed-panels: [Demo](http://daniel-hug.github.io/tabbed-panels/)
 =============
 
-A tabbed panel switcher JS script
+A declarative tabbed panels JS module
+
+ - lightweight
+ - no dependencies
+ - no exports
+
+## Use
+
+1. Include **panels.js** and **panels.css**.
+
+2. Wrap the tabs/buttons and their panels in a `.tabbed-panels` wrapper, and add a class of "**tab**" to each tab and "**panel**" to each panel:
+
+    ```html
+    <div class="tabbed-panels">
+        <!-- Tabs -->
+        <button class="tab">Tab 1</button>
+        <button class="tab">Tab 2</button>
+
+        <!-- Panels -->
+        <div class="panel">
+            <h1>Panel 1</h1>
+        </div>
+        <div class="panel">
+            <h1>Panel 2</h1>
+        </div>
+    </div>
+    ```
+
+## Options
+
+ - To make panels closeable add a "**closeable**" class to the `.tabbed-panels` wrapper.
+ - To make panels hover over other page content add a "**hovering**" class to the `.tabbed-panels` wrapper. Combining the closeable and hovering classes will make the panels close when you click elsewhere on the page.
+ - To have a panel defaultly be open, add an "**active**" class to the panel, its tab, and its `.tabbed-panels` wrapper.
+
+## Styling
+
+You can use panels.css and/or your own custom CSS. Whenever a panel is open, it, its tab, and its `.tabbed-panels` wrapper all have an "**active**" class.
 
 ## Browser support
-Requires `querySelectorAll`, `classList`, and `forEach`.
+Requires `getElementsByClassName`, `classList`, `forEach`, and `filter`.
--- bower_components/tabbed-panels/panels.js
+++ bower_components/tabbed-panels/panels.js
@@ -3,26 +3,45 @@
 	var tabs = this('tab', this('tabs', parent)[0] || parent);
 	var panels = this('panel', parent);
 	var closeable = parent.classList.contains('closeable');
+	var hovering = parent.classList.contains('hovering');
+	var lastClickedTab = null;
+	var lastClickedTabI = null;
+
+	function tabClick(i) {
+		lastClickedTab = this;
+		lastClickedTabI = i;
+
+		// non-clicked tabs: deactivate
+		tabs.forEach(function(tab) {
+			if (tab != this) tab.classList.remove('active');
+		}, this);
+
+		// clicked tab: if closeable, toggle its active state, otherwise activate
+		var isActive = this.classList[closeable ? 'toggle' : 'add']('active');
+
+		// parent: if not closeable or if a tab is active, activate parent, otherwise deactivate
+		parent.classList[(!closeable || isActive) ? 'add' : 'remove']('active');
+
+		// panels: activate clicked tab's panel, deactivate other panels
+		panels.forEach(function(panel, j) {
+			panel.classList[j === i ? (closeable ? 'toggle' : 'add') : 'remove']('active');
+		});
+	}
 
 	tabs.forEach(function(tab, i) {
-		tab.addEventListener('click', function() {
-			tabs.forEach(function(tab) {
-				if (tab != this) tab.classList.remove('active');
-			}, this);
-
-			if (closeable) {
-				var isActive = this.classList.toggle('active');
-				parent.classList[isActive ? 'add' : 'remove']('active');
-			} else {
-				this.classList.add('active');
-				parent.classList.add('active');
-			}
-
-			panels.forEach(function(panel, j) {
-				panel.classList[j === i ? (closeable ? 'toggle' : 'add') : 'remove']('active');
-			});
-		});
+		tab.addEventListener('click', tabClick.bind(tab, i));
 	});
+
+	if (closeable && hovering) {
+		window.addEventListener('click', function(e) {
+			if (
+				parent.classList.contains('active') &&
+				(!parent.contains(e.target) || e.target === parent)
+			) {
+				tabClick.call(lastClickedTab, lastClickedTabI);
+			}
+		});
+	}
 
 // Get children with class:
 }, function(className, scopeEl) {
